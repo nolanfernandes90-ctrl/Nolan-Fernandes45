ff468b7e1510990292f09c64ad2efebe84ce4e5d
Flashlab Creative <cybercreative@MacBookAir.lan>
2026-08-28 17:44:08 +0530

    ain

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Thegoamonitorreact/', // Set the base path for your app
+  base: '/Thegoamonitorreact/',
 })
